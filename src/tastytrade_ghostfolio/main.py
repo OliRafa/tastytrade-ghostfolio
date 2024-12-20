61a0d14ba1ec2697b44bdaf0eb4869276ba8e710
Rafael Augusto de Oliveira <olirafa@proton.me>
2024-12-20 08:37:10 +0000

    test(main): fix pytest invoking the whole script when first importing it

--- src/tastytrade_ghostfolio/main.py
+++ src/tastytrade_ghostfolio/main.py
@@ -77,55 +77,57 @@ def adapt_symbols(
 # # response_data["activities"], tastytrade_account["id"]
 # # )
 
+if __name__ == "__main__":
+    ghostfolio_service = GhostfolioService()
+    ghostfolio_accounts = ghostfolio_service.get_all_accounts()
 
-ghostfolio_service = GhostfolioService()
-ghostfolio_accounts = ghostfolio_service.get_all_accounts()
-
-try:
-    tastytrade_account = next(
-        filter(
-            lambda x: x.name.lower() in ["tastytrade", "tastyworks"],
-            ghostfolio_accounts,
-        )
-    )
-
-except StopIteration:
-    tastytrade_account = GhostfolioAccount(name="Tastytrade")
-    tastytrade_account = ghostfolio_service.create_account(tastytrade_account)
-
-session = get_tastytrade_session()
-transactions = get_tastytrade_account_history(session)
-
-trades = filter_trades(transactions)
-activities = adapt_trades(trades)
-
-for activity in activities:
-    activity.account_id = tastytrade_account.account_id
-    activity.comment = "Activity created by Tastytrade-Ghostfolio."
-
-orders = ghostfolio_service.get_all_orders(tastytrade_account.account_id)
-buy_orders = list(filter(lambda x: x.type == TransactionType.BUY, orders))
-
-print("Started exporting activities to Ghostfolio...")
-activities_for_exporting: list[GhostfolioActivity] = []
-for activity in activities:
     try:
-        next(
+        tastytrade_account = next(
             filter(
-                lambda x: x.date == activity.date
-                and x.quantity == activity.quantity
-                and x.symbol == activity.symbol,
-                buy_orders,
+                lambda x: x.name.lower() in ["tastytrade", "tastyworks"],
+                ghostfolio_accounts,
             )
         )
+
     except StopIteration:
-        activities_for_exporting.append(activity)
+        tastytrade_account = GhostfolioAccount(name="Tastytrade")
+        tastytrade_account = ghostfolio_service.create_account(tastytrade_account)
 
-try:
-    symbol_mappings = SymbolMappingRepository().get_symbol_mappings()
-    activities_for_exporting = adapt_symbols(activities_for_exporting, symbol_mappings)
-except SymbolMappingsNotFoundException:
-    print("Skipping symbol changes, as no mapping file was found.")
+    session = get_tastytrade_session()
+    transactions = get_tastytrade_account_history(session)
 
-ghostfolio_service.export_activities_to_ghostfolio(activities_for_exporting)
-print("Done!")
+    trades = filter_trades(transactions)
+    activities = adapt_trades(trades)
+
+    for activity in activities:
+        activity.account_id = tastytrade_account.account_id
+        activity.comment = "Activity created by Tastytrade-Ghostfolio."
+
+    orders = ghostfolio_service.get_all_orders(tastytrade_account.account_id)
+    buy_orders = list(filter(lambda x: x.type == TransactionType.BUY, orders))
+
+    print("Started exporting activities to Ghostfolio...")
+    activities_for_exporting: list[GhostfolioActivity] = []
+    for activity in activities:
+        try:
+            next(
+                filter(
+                    lambda x: x.date == activity.date
+                    and x.quantity == activity.quantity
+                    and x.symbol == activity.symbol,
+                    buy_orders,
+                )
+            )
+        except StopIteration:
+            activities_for_exporting.append(activity)
+
+    try:
+        symbol_mappings = SymbolMappingRepository().get_symbol_mappings()
+        activities_for_exporting = adapt_symbols(
+            activities_for_exporting, symbol_mappings
+        )
+    except SymbolMappingsNotFoundException:
+        print("Skipping symbol changes, as no mapping file was found.")
+
+    ghostfolio_service.export_activities_to_ghostfolio(activities_for_exporting)
+    print("Done!")
